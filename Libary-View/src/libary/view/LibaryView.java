183fc447be7d423d5bbdd846a3f878307675ee71
Felipe Rabelo <carbonemasseo@hotmail.com>
2015-10-19 08:25:08 -0200

    Firsts interfaces
    Modelo de criação das 3 primeiras e mais importantes interfaces iniciais
    do projeto

---
+++ Libary-View/src/libary/view/LibaryView.java
@@ -0,0 +1,24 @@
+
+
+package libary.view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Felipe Rabelo
+ */
+public class LibaryView {
+
+    public static void main(String[] args) {
+        Cadastro_Aluno win1 = new Cadastro_Aluno ();win1.setVisible(true);
+        win1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Cadastro_Livro win2 = new Cadastro_Livro ();win2.setVisible(true);
+        win2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        Cadastro_Master win3 = new Cadastro_Master ();win3.setVisible(true);
+        win3.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Libary</h1>
+
+William pau no cu, não sabe porra nenhuma e fica pedindo coisa que nem ele sabe fazer e eu tenho que tapar esses buraco ¬¬.
