75ba2cbf8a3cf2e27bdb6b1678ac16cf91b78d62
diegobsilveira <diegobsilveira@gmail.com>
2015-10-26 17:54:44 -0200

    Test

---
+++ Libary-View/src/Library-View-TESTE/Lista_Livros_Controlador.java
@@ -0,0 +1,181 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width">
+  <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1" media="(device-height: 568px)">
+  <meta http-equiv="Content-Language" content="en">
+  <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114-precomposed.png">
+    <meta name="browser-stats-url" content="https://api.github.com/_private/browser/stats">
+  <meta name="theme-color" content="#f5f5f5">
+
+      <meta name="google-analytics" content="UA-3769691-2">
+
+<meta content="collector.githubapp.com" name="octolytics-host" /><meta content="github" name="octolytics-app-id" /><meta content="BD2853FA:7D70:68EB58A:562E47CC" name="octolytics-dimension-request_id" /><meta content="15195883" name="octolytics-actor-id" /><meta content="diegobsilveira" name="octolytics-actor-login" /><meta content="304ec9f68585984b48880cc548fbbac47a63640c6a5be36cb8b778ea7a7ac8f2" name="octolytics-actor-hash" />
+<meta content="mobile" name="octolytics-dimension-device" />
+<meta content="9655696" name="octolytics-dimension-user_id" /><meta content="KHs000" name="octolytics-dimension-user_login" /><meta content="44527264" name="octolytics-dimension-repository_id" /><meta content="KHs000/Library" name="octolytics-dimension-repository_nwo" /><meta content="true" name="octolytics-dimension-repository_public" /><meta content="false" name="octolytics-dimension-repository_is_fork" /><meta content="44527264" name="octolytics-dimension-repository_network_root_id" /><meta content="KHs000/Library" name="octolytics-dimension-repository_network_root_nwo" />
+
+
+<meta content="Rails, view, blob#show" data-pjax-transient="true" name="analytics-event" />
+
+
+  <meta class="js-ga-set" name="dimension1" content="Logged In">
+    <meta class="js-ga-set" name="dimension3" content="mobile">
+
+
+
+
+  <title>GitHub · Where software is built</title>
+
+  <link crossorigin="anonymous" href="https://assets-cdn.github.com/assets/mobile-aa34a6507f102373ed6ec75065f8dfca44d4e0e9aa821330242d02c15e83cf45.css" integrity="sha256-qjSmUH8QI3PtbsdQZfjfykTU4OmqghMwJC0CwV6Dz0U=" media="all" rel="stylesheet" />
+  <script async="async" crossorigin="anonymous" integrity="sha256-RuvPh+frWh163mKDRVKUU9nsEx37wq4Ow4XjJTQj4l8=" src="https://assets-cdn.github.com/assets/mobile-46ebcf87e7eb5a1d7ade628345529453d9ec131dfbc2ae0ec385e3253423e25f.js"></script>
+</head>
+<body class="">
+  <header class="nav-bar">
+    <div class="nav-bar-inner">
+      
+
+      <button class="header-button header-nav-button touchable js-show-global-nav">
+        <span class="octicon octicon-three-bars"></span>
+
+      </button>
+
+      <div class="nav-bar-title-text">
+              <span class="octicon octicon-repo"></span>
+
+    <strong><a href="/KHs000">KHs000</a></strong>
+    /
+    <strong><a href="/KHs000/Library">Library</a></strong>
+
+      </div>
+    </div>
+
+
+    <nav class="nav-bar-tabs repo-nav-bar-tabs">
+      <ul>
+              <li>
+                <a href="/notifications">
+                  <span class="octicon octicon-bell"></span>
+                  Notifications
+                </a>
+              </li>
+            <li>
+              <a href="/">
+                <span class="octicon octicon-dashboard"></span>
+                Dashboard
+              </a>
+            </li>
+          <li>
+            <a href="/explore">
+              <span class="octicon octicon-telescope"></span>
+              Explore
+            </a>
+          </li>
+            <li>
+              <a href="/diegobsilveira">
+                <img alt="@diegobsilveira" class="avatar" height="16" src="https://avatars3.githubusercontent.com/u/15195883?v=3&amp;s=32" width="16" />
+                Profile
+              </a>
+            </li>
+        <li>
+            <a href="/logout">
+              <span class="octicon octicon-sign-out"></span>
+              Sign out
+            </a>
+        </li>
+            <li class="section-title">This repository</li>
+    <li><a href="/KHs000/Library?files=1"><span class="octicon octicon-code"></span> Code</a></li>
+      <li><a href="/KHs000/Library/issues"><span class="octicon octicon-issue-opened"></span> Issues</a></li>
+    <li><a href="/KHs000/Library/pulls"><span class="octicon octicon-git-pull-request"></span> Pull Requests</a></li>
+    <li><a href="/KHs000/Library/pulse"><span class="octicon octicon-pulse"></span> Pulse</a></li>
+
+      </ul>
+    </nav>
+  </header>
+
+    
+<div class="flash flash-full js-notice flash-warn">
+<div class="container">
+      <h4 class="flash-content">
+        You don’t have any verified emails.  We recommend <a href="https://github.com/settings/emails">verifying</a> at least one email.
+      </h4>
+      Email verification helps our support team verify ownership if you lose account access
+        and allows you to receive all the notifications you ask for.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+
+
+  <div id="js-flash-container">
+</div>
+
+
+        
+
+<div class="breadcrumb blob-breadcrumb">
+  <label for="blob-history-checkbox" class="blob-history-label">
+    <span class="octicon octicon-history"></span>
+  </label>
+  <span class="filetype-icon"><span class="octicon octicon-file-text"></span></span>
+  <span itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="/KHs000/Library/tree/master/Libary-View" class="" data-branch="master" data-pjax="true" itemscope="url"><span itemprop="title">Libary-View</span></a></span><span class="separator">/</span><span itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="/KHs000/Library/tree/master/Libary-View/src" class="" data-branch="master" data-pjax="true" itemscope="url"><span itemprop="title">src</span></a></span><span class="separator">/</span><span itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="/KHs000/Library/tree/master/Libary-View/src/library" class="" data-branch="master" data-pjax="true" itemscope="url"><span itemprop="title">library</span></a></span><span class="separator">/</span><span itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="/KHs000/Library/tree/master/Libary-View/src/library/view" class="" data-branch="master" data-pjax="true" itemscope="url"><span itemprop="title">view</span></a></span><span class="separator">/</span><strong class="final-path">Lista_Livros_Controlador.java</strong>
+</div>
+
+<input id="blob-history-checkbox"
+       class="js-blob-history-checkbox"
+       type="checkbox"
+       data-url="/KHs000/Library/latest_commit/master/Libary-View/src/library/view/Lista_Livros_Controlador.java">
+
+<div class="blob-history">
+  <a href="/KHs000/Library/commits/master/Libary-View/src/library/view/Lista_Livros_Controlador.java" class="js-blob-history-link">
+    Loading latest commit...
+</a></div>
+
+  <div class="blob-file-content js-file-line-container">
+    <div class="highlighted-blob tab-size" data-tab-size="8"><div class="code-body highlight"><pre><div class='line js-file-line' id='LC1'><br></div><div class='line js-file-line' id='LC2'><br></div><div class='line js-file-line' id='LC3'><span class="pl-k">package</span> <span class="pl-smi">library.view</span>;</div><div class='line js-file-line' id='LC4'><br></div><div class='line js-file-line' id='LC5'><span class="pl-k">import</span> <span class="pl-smi">javax.swing.table.DefaultTableModel</span>;</div><div class='line js-file-line' id='LC6'><br></div><div class='line js-file-line' id='LC7'><span class="pl-c">/**</span></div><div class='line js-file-line' id='LC8'><span class="pl-c"> *</span></div><div class='line js-file-line' id='LC9'><span class="pl-c"> * @author Felipe Rabelo</span></div><div class='line js-file-line' id='LC10'><span class="pl-c"> */</span></div><div class='line js-file-line' id='LC11'><span class="pl-k">public</span> <span class="pl-k">class</span> <span class="pl-en">Lista_Livros_Controlador</span> <span class="pl-k">extends</span> <span class="pl-e">Lista_Livros</span>{</div><div class='line js-file-line' id='LC12'>&nbsp;&nbsp;&nbsp;&nbsp;</div><div class='line js-file-line' id='LC13'>&nbsp;&nbsp;&nbsp;&nbsp;<span class="pl-k">public</span> <span class="pl-en">Lista_Livros_Controlador</span> () {}</div><div class='line js-file-line' id='LC14'>&nbsp;&nbsp;&nbsp;&nbsp;</div><div class='line js-file-line' id='LC15'>&nbsp;&nbsp;&nbsp;&nbsp;<span class="pl-k">public</span> <span class="pl-k">void</span> <span class="pl-en">mount</span> () {</div><div class='line js-file-line' id='LC16'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div><div class='line js-file-line' id='LC17'>&nbsp;&nbsp;&nbsp;&nbsp;}</div><div class='line js-file-line' id='LC18'>}</div></pre></div></div>
+  </div>
+
+
+
+  <footer class="clearfix">
+    <div class="container">
+      <a href="#"><span class="mega-octicon octicon-mark-github"></span></a>
+
+      <ul class="clearfix">
+        <li>
+          <!-- </textarea> --><!-- '"` --><form accept-charset="UTF-8" action="/site/mobile_preference" class="js-mobile-preference-form" data-form-nonce="9373eee38aeb69b58adb87f49a736723a3d733b0" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="KcG7xQYxAbUW0EYsbFTnbAQ0+hSgHOpK31Reli4W8CzTxTz57l2Z54lGLg73lUQs52/2qGfBhZBSQKmFLKV8gQ==" /></div>
+            <input type="hidden" name="mobile" value="false">
+            <input type="hidden" name="anchor" class="js-mobile-preference-anchor-field">
+
+            <button type="submit" class="switch-to-desktop" data-ga-click="Mobile, switch to desktop, switch button">
+              Desktop version
+            </button>
+</form>        </li>
+
+      </ul>
+    </div>
+  </footer>
+  
+</body>
+</html>
+
